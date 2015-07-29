b482d9dc4173edd6b7c127c680ff68325699342e
Andrew Kos <akos@theonion.com>
2015-07-29 16:35:54 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+name = "bulbs-component-external-link"
+version = "0.0.1"
+
+requires = [
+    "django-bulbs==0.6.1",
+]
+
+dev_requires = [
+    "virtualenv==13.0.3"
+]
+
+setup(
+    name=name,
+    version=version,
+    description="External Link component for bulbs.",
+    license="MIT",
+    author="Andrew Kos",
+    author_email="akos@theonion.com",
+    package_dir={
+        "bulbs_component_external_link": "src/django-bulbs",
+    },
+    packages=[
+        "bulbs_component_external_link",
+    ],
+    include_package_data=True,
+    install_requires=requires,
+    extras_require={
+        "dev": dev_requires
+    }
+)
